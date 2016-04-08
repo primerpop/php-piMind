0e99be1fd85e2872e48bd767ed1a1c3fc1e86ebf
primerpop <skinnepa@hotmail.com>
2016-04-07 23:53:45 -0400

    initial

---
+++ controller/handlers/alarm.php
@@ -0,0 +1,43 @@
+<?php
+/**
+* handler_template - piMind Controller extension
+* Describe your handler
+*
+* @author Paul
+*
+*/
+
+class alarm extends controller_handler {
+	protected $_controller = null;
+	
+	public function create($controller_pointer) {
+		$this->_controller = $controller_pointer;
+		$this->_controller->log("Alarm Handler initialized");
+		return 1;
+	}
+	public function destroy() {
+
+	}
+	public function event($data) {
+		if (isset($data->type)) {
+			switch ($data->type) {
+				case EVENT_TYPE_SENSOR:
+					break;
+				case EVENT_TYPE_HANDLER:
+					if ($data->event_code == 10) {
+						$this->_controller->log("Alarm Service sees an event coded with event_code 10");
+					}
+						
+					break;
+				case EVENT_TYPE_VARDATA:
+					break;
+			}
+			
+		}
+	}
+	public function tick() {
+	
+	}
+
+
+}
