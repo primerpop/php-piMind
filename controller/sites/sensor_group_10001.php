73524f578c250309a07c89d10d8aad97232c6c09
primerpop <skinnepa@hotmail.com>
2016-04-11 21:54:33 -0400

    test

--- controller/sites/sensor_group_10001.php
+++ controller/sites/sensor_group_10001.php
@@ -1,12 +1,32 @@
 <?php
 
 class sensor_group_10001 {
+	protected $_controller = null;
 	
-	public function event($message) {
-		print_r($message);
-		
+	public function create($controller_pointer) {
+		$this->_controller = $controller_pointer;
+		$this->_controller->log("handler template initialized");
+		return 1;
 	}
+	public function destroy() {
 	
+	}
+	public function event($data) {
+		if (isset($data->type)) {
+			switch ($data->type) {
+				case EVENT_TYPE_SENSOR:
+					break;
+				case EVENT_TYPE_HANDLER:
+					break;
+				case EVENT_TYPE_VARDATA:
+					break;
+			}
+				
+		}
+	}
+	public function tick() {
+	
+	}
 	
 }
 
