919eed23fad7626d46b11f656c7f7755c5f54892
primerpop <skinnepa@hotmail.com>
2016-06-27 18:50:35 -0400

    helps to read in a file

--- www/state.php
+++ www/state.php
@@ -4,6 +4,6 @@ include("../paths.php");
 // get constants
 include(PIMIND_HOME."/constants.php");
 
-$state = json_decode(PIMIND_STATE .DIRECTORY_SEPARATOR . "controller-state-0.json",1);
+$state = json_decode(file_get_contents(PIMIND_STATE .DIRECTORY_SEPARATOR . "controller-state-0.json"),1);
 var_dump($state);
 ?>
